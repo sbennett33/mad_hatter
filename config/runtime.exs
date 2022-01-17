80b5cd825b53ae41b225223ef78a304a22d1ee54
Scott Bennett <sbennett@change.org>
2022-01-17 08:29:27 -0800

    Enable compression

--- config/runtime.exs
+++ config/runtime.exs
@@ -27,7 +27,8 @@ if config_env() == :prod do
     url: [host: "#{app_name}.fly.dev", port: 80],
     http: [
       port: String.to_integer(System.get_env("PORT") || "4000"),
-      ip: {0, 0, 0, 0, 0, 0, 0, 0}
+      ip: {0, 0, 0, 0, 0, 0, 0, 0},
+      compress: true
     ],
     server: true,
     secret_key_base: secret_key_base
